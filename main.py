2bffb4cc8a622ba233a6231f5e726eaccc4159f0
The-6th-Champion <67034744+The-6th-Champion@users.noreply.github.com>
2021-02-03 16:22:10 -0800

    changed offset
    now takes input from the user, which I should have done much earlier
    The old line of code is still there (but commented) for those that wanna play around with the code anyways

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #importing time. duh
 import time
 
-#hours offset: change this to go forwards (positive0 or backwards (negative)
-offset = 0 
+#hours offset: change this to go forwards (positive) or backwards (negative)
+offset = int(input("Offset from your current time in hours (put 0 if none, negative number if back in time, etc): ")) 
+#offset = 0   old line of code
 
 #quick maths with offset
 timezone = 60*60*offset
@@ -19,3 +20,4 @@ print(utc_ts)
 
 
 #Comments are fun
+#comment in the comment section if you think so too
